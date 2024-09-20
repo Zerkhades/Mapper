1ea4073f12fce1a31c0991085c5c85b73440e67c
makarov_ta <makarov_ta@electronxray.com>
2024-09-20 16:35:11 +0300

    I hate cqrs

---
+++ Mapper.Application/CommandsAndQueries/GeoMap/Queries/GetGeoMapDetails/GeoMapDetailsVm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Mapper.Application.CommandsAndQueries.GeoMap.Commands.CreateGeoMapCommand;
+using Mapper.Domain;
+
+namespace Mapper.Application.CommandsAndQueries.GeoMap.Queries.GetGeoMapDetails
+{
+    internal class GeoMapDetailsVm
+    {
+        public Guid Id { get; set; }
+        public string MapName { get; set; }
+        public string MapDescription { get; set; }
+        public byte[] Map { get; set; }
+        public bool IsArchived { get; set; }
+        public virtual ObservableCollection<GeoMark>? GeoMarks { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.GeoMap, CreateGeoMapCommand>()
+                .ForMember(mapCommand => mapCommand.MapName,
+                    opt => opt.MapFrom(mapDto => mapDto.MapName))
+                .ForMember(mapCommand => mapCommand.MapDescription,
+                    opt => opt.MapFrom(mapDto => mapDto.MapDescription))
+                .ForMember(mapCommand => mapCommand.Map,
+                    opt => opt.MapFrom(mapDto => mapDto.Map))
+                .ForMember(mapCommand => mapCommand.IsArchived,
+                    opt => opt.MapFrom(mapDto => mapDto.IsArchived));
+        }
+}
---
+++ Mapper.Application/CommandsAndQueries/GeoMap/Queries/GetGeoMapDetails/GetGeoMapDetailsQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper.Application.CommandsAndQueries.GeoMap.Queries.GetGeoMapDetails
+{
+    internal class GetGeoMapDetailsQueryValidator : AbstractValidator<GetGeoMapDetailsQuery>
+    {
+        public GetGeoMapDetailsQueryValidator() 
+        {
+            RuleFor(note => note.Id).NotEqual(Guid.Empty);
+        }
+    }
+}
