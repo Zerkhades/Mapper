4390f96de355a080765015b2ae37c960374ca08b
Zerkhades <zerkhade@gmail.com>
2025-03-30 11:40:16 +0300

    Изменения в тестах для команд удаления и обновления сотрудников
    Изменены классы тестов `DeleteEmployeeCommandHandlerTests` и `UpdateEmployeeCommandHandlerTests` с внутреннего на публичный. Добавлены тесты для успешного удаления и обновления сотрудников, а также для обработки ошибок при неправильном идентификаторе. В файле `docker-compose.dcproj` добавлено свойство `<AutoAddFiles>false</AutoAddFiles>`.

--- Mapper.Tests/Employees/Commands/DeleteEmployeeCommandHandlerTests.cs
+++ Mapper.Tests/Employees/Commands/DeleteEmployeeCommandHandlerTests.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Mapper.Application.CommandsAndQueries.Employee.Commands.DeleteEmployeeCommand;
+using Mapper.Application.Common.Exceptions;
+using Mapper.Tests.Common;
+using Mapper.Tests.Common.ContextFactories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mapper.Tests.Employees.Commands
 {
-    internal class DeleteEmployeeCommandHandlerTests
+    public class DeleteEmployeeCommandHandlerTests : TestCommandBase
     {
+        public DeleteEmployeeCommandHandlerTests() : base(new EmployeesContextFactory())
+        {
+        }
+
+        [Fact]
+        public async Task DeleteEmployeeCommandHandler_Success()
+        {
+            // Arrange
+            var handler = new DeleteEmployeeCommandHandler(Context);
+            var employeeId = EmployeesContextFactory.EmployeeIdForDelete;
+
+            // Act
+            await handler.Handle(
+                new DeleteEmployeeCommand
+                {
+                    Id = employeeId
+                },
+                CancellationToken.None);
+
+            // Assert
+            Assert.Null(
+                await Context.Employees.SingleOrDefaultAsync(employee =>
+                    employee.Id == employeeId));
+        }
+
+        [Fact]
+        public async Task DeleteEmployeeCommandHandler_FailOnWrongId()
+        {
+            // Arrange
+            var handler = new DeleteEmployeeCommandHandler(Context);
+            var wrongId = Guid.NewGuid();
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<NotFoundException>(async () =>
+                await handler.Handle(
+                    new DeleteEmployeeCommand
+                    {
+                        Id = wrongId
+                    },
+                    CancellationToken.None)
+            );
+        }
     }
 }
--- Mapper.Tests/Employees/Commands/UpdateEmployeeCommandHandlerTests.cs
+++ Mapper.Tests/Employees/Commands/UpdateEmployeeCommandHandlerTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Mapper.Application.CommandsAndQueries.Employee.Commands.UpdateEmployeeCommand;
+using Mapper.Application.Common.Exceptions;
+using Mapper.Tests.Common;
+using Mapper.Tests.Common.ContextFactories;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,50 @@ using System.Threading.Tasks;
 
 namespace Mapper.Tests.Employees.Commands
 {
-    internal class UpdateEmployeeCommandHandlerTests
+    public class UpdateEmployeeCommandHandlerTests : TestCommandBase
     {
+        public UpdateEmployeeCommandHandlerTests() : base(new EmployeesContextFactory())
+        {
+        }
+
+        [Fact]
+        public async Task UpdateEmployeeCommandHandler_Success()
+        {
+            // Arrange
+            var handler = new UpdateEmployeeCommandHandler(Context);
+            var updatedFirstName = "Booba";
+            var updatedSurname = "Job";
+
+            // Act
+            await handler.Handle(new UpdateEmployeeCommand
+            {
+                Id = EmployeesContextFactory.EmployeeIdForUpdate,
+                FirstName = updatedFirstName,
+                Surname = updatedSurname
+            }, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(await Context.Employees.SingleOrDefaultAsync(employee =>
+                employee.Id == EmployeesContextFactory.EmployeeIdForUpdate &&
+                employee.FirstName == updatedFirstName &&
+                employee.Surname == updatedSurname));
+        }
+
+        [Fact]
+        public async Task UpdateEmployeeCommandHandler_FailOnWrongId()
+        {
+            // Arrange
+            var handler = new UpdateEmployeeCommandHandler(Context);
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<NotFoundException>(async () =>
+                await handler.Handle(
+                    new UpdateEmployeeCommand
+                    {
+                        Id = Guid.NewGuid(),
+                    },
+                    CancellationToken.None));
+        }
     }
 }
